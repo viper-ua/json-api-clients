2f2d0f2b1bb42476157c471899a962d3a781572a
Vladimir Zhilyaev <vladimir@zhilyaev.kyiv.ua>
2017-11-13 09:53:14 +0200

    Refactoring of api_client

--- api_client.rb
+++ api_client.rb
@@ -1,113 +1,80 @@
 require 'net/http'
 require 'json'
 
-def get_product_list
-  uri = URI "http://localhost:3000/products"
+SERVER = 'http://localhost:3000'
 
-  req = Net::HTTP::Get.new(uri)
+def http_request_json(http_method, uri, body = nil)
+  # Checking HTTP method to execute
+  http_method.upcase!
+  case http_method
+  when 'PUT'
+    req = Net::HTTP::Put.new(uri)
+  when 'GET'
+    req = Net::HTTP::Get.new(uri)
+  when 'POST'
+    req = Net::HTTP::Post.new(uri)
+  when 'DELETE'
+    req = Net::HTTP::Delete.new(uri)
+  end
+  # Generating request
   req['Accept'] = 'application/json'
-
+  req['Content-Type'] = 'application/json'
+  req.body = JSON.generate(body) if body
+  # Generating response back to caller
   res = Net::HTTP.start(uri.hostname, uri.port) do |http|
     http.request(req)
-  end
-  json_data = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
-  puts "Trying to get product list..."
-  puts "Response: #{res.code}:#{res.message}"
+  end 
+end 
 
-  if json_data
-    puts "JSON returned #{json_data.length} records on top level: "
-    json_data.each do |product|
-      puts "id:#{product['id']} -> #{product['title']} ($#{product['price']})"
-    end
-  end
-  puts
+def get_product_list
+  uri = URI "#{SERVER}/products"
+
+  res = http_request_json('get', uri)
+  json_data = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
 end
 
 def get_product(product_id)
-  uri = URI "http://localhost:3000/products/#{product_id}"
+  uri = URI "#{SERVER}/products/#{product_id}"
 
-  req = Net::HTTP::Get.new(uri)
-  req['Accept'] = 'application/json'
-
-  res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-    http.request(req)
-  end
+  res = http_request_json('get', uri)
   json_data = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
-  puts "Trying to get product with id:#{product_id}"
-  puts "Response: #{res.code}:#{res.message}"
-
-  if json_data
-    puts "JSON returned #{json_data.length} records on top level: "
-    puts "id:#{json_data['id']} -> #{json_data['title']} ($#{json_data['price']})"
-  end
-  puts
 end
 
 def update_product(product_id, product)
-  uri = URI "http://localhost:3000/products/#{product_id}.json"
+  uri = URI "#{SERVER}/products/#{product_id}"
 
-  req = Net::HTTP::Put.new(uri)
-  req['Accept'] = 'application/json'
-  req['Content-Type'] = 'application/json'
-  req.body = JSON.generate(product)
-
-  res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-    http.request(req)
-  end
-  json_data = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
-  puts "Updating product id:#{product_id}"
-  puts "Response: #{res.code}:#{res.message}"
-
-  if json_data
-    puts "JSON returned #{json_data.length} records on top level: "
-    puts "id:#{json_data['id']} -> #{json_data['title']} ($#{json_data['price']})"
-  end
-  puts
+  res = http_request_json('put', uri, product)
+  (res.code == '200')
 end
 
 def create_product(product)
-  uri = URI "http://localhost:3000/products.json"
+  uri = URI "#{SERVER}/products"
 
-  req = Net::HTTP::Post.new(uri)
-  req['Accept'] = 'application/json'
-  req['Content-Type'] = 'application/json'
-  req.body = JSON.generate(product)
-
-  res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-    http.request(req)
+  res = http_request_json('post', uri, product)
+  
+  if res.is_a?(Net::HTTPSuccess)
+    @new_product_id = JSON.parse(res.body)['id']
   end
-  json_data = JSON.parse(res.body) if res.is_a?(Net::HTTPSuccess)
-  puts "Trying to create new test product"
-  puts "Response: #{res.code}:#{res.message}"
-
-  if json_data
-    puts "JSON returned #{json_data.length} records on top level: "
-    puts "id:#{json_data['id']} -> #{json_data['title']} ($#{json_data['price']})"
-    @new_product_id = json_data['id']
-  end
-  puts
 end
 
 def delete_product(product_id)
-  uri = URI "http://localhost:3000/products/#{product_id}.json"
+  uri = URI "#{SERVER}/products/#{product_id}"
 
-  req = Net::HTTP::Delete.new(uri)
-  req['Accept'] = 'application/json'
-  req['Content-Type'] = 'application/json'
-
-  res = Net::HTTP.start(uri.hostname, uri.port) do |http|
-    http.request(req)
-  end
-  puts "Trying to delete test product with id:#{product_id}"
-  puts "Response: #{res.code}:#{res.message}"
-  puts
+  res = http_request_json('delete', uri)
+  (res.code == '204')
 end
 
-get_product_list
-get_product(11)
-create_product({"title": "New book just for tests",
-              "description": "This is a new book, used for test purposes only",
-              "price": 45.0, 
-              "image_url": "rails.png"})
-update_product(11,{"title": "Rails, Angular, Postgres, and Bootstrap","price": 45.0})
-delete_product(@new_product_id)
+# Running client
+puts "\r\n--- Getting product list ---"
+puts get_product_list
+puts "\r\n--- Getting product with id:11 ---"
+puts get_product(11)
+puts "\r\n--- Creating new product ---"
+puts create_product({title: "New book just for tests", 
+              description: "This is a new book, used for test purposes only", 
+              price: 45.0, 
+              image_url: "rails.png"})
+puts "\r\n--- Updating product ---"
+puts update_product(11,{title: "Rails, Angular, Postgres, and Bootstrap", price: 45.0})
+puts "\r\n--- Deleting created product id:#{@new_product_id} ---"
+puts delete_product(@new_product_id)
